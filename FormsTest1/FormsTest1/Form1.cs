f01a62b3eef03085064ee7d3d5ae405333881c1a
getgoh <blujaygo@gmail.com>
2017-01-09 10:24:37 -0500

    1

---
+++ FormsTest1/FormsTest1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+        
+
+        private void label1_LocationChanged(object sender, EventArgs e)
+        {
+            label1.Text = "X = " + label1.Location.X + ", Y = " + label1.Location.Y;
+        }
+
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            label1.Location = Cursor.Position;
+        }
+
+        private void label1_DragOver(object sender, DragEventArgs e)
+        {
+            label1.Location = Cursor.Position;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            label1.Text = "W = " + Form1.ActiveForm.Width + ", H = " + Form1.ActiveForm.Height;
+        }
+    }
+}
---
+++ SimpleDelegate/SimpleDelegate/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    class Program
+    {
+        public delegate void Del(string message);
+        public delegate int Add(int num1, int num2);
+
+        public delegate Student Stud(string firstname, string lastname, char gender);
+
+        static void Main(string[] args)
+        {
+            Del handler2 = DelegateMethod;
+            Del handler = DelegateMethod2;
+            Add handler3 = AddNumbers;
+            Stud handler4 = AddTitle;
+
+            // handler.invoke(parameters)
+            handler2.Invoke("test");
+            int sum = handler3.Invoke(50, 25);
+
+            // handler(parameters)
+            int sum2 = handler3(20, 5);
+            handler("Al");
+            Student s = handler4("Al", "Goh", 'M');
+
+            Console.WriteLine(s.Fullname);
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Sum2 = " + sum2);
+        }
+
+        public static void DelegateMethod(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public static void DelegateMethod2(string name)
+        {
+            System.Console.WriteLine("Hello " + name);
+        }
+
+        public static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static Student AddTitle(string firstname, string lastname, char gender)
+        {
+            string title = gender == 'M' ? "Mr. " : "Ms. ";
+            return new Student() { Fullname = title + firstname + " " + lastname};            
+        }
+    }
+
+    public class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public char Gender { get; set; }
+        public string Fullname { get; set; }
+    }
+}
