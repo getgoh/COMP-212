8e46cf2a2ea29e713dd7cb2039684d0f57144fd2
getgoh <blujaygo@gmail.com>
2017-02-15 12:45:49 -0500

    Wx

---
+++ DisplayQueryResult/DisplayQueryResult/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayQueryResult
+{
+    public partial class Form1 : Form
+    {
+        private BookExamples.BooksEntities ctx = new BookExamples.BooksEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ctx.Titles.Load();
+            titleBindingSource.DataSource = ctx.Titles.Local;
+
+            cbQueries.SelectedIndex = 0;
+        }
+
+        private void cbQueries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
