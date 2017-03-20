c02e0e115a5c0797f9cbaf90c06e712dfa7a7114
getgoh <blujaygo@gmail.com>
2017-03-20 18:13:36 -0400

    Assignment 4
    Question 2 done!

---
+++ 300910584(goh)_ASS_4/Goh_Q2/Goh_Q2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedListLibrary;
+
+namespace Goh_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiate sorted lists for int
+            SortedList sl1 = new SortedList("slInt1");
+            SortedList sl2 = new SortedList("slInt2");
+
+            // Instantiate sorted list for string
+            SortedList slString = new SortedList("slString");
+
+            // populate string sorted list
+            slString.Add("Al");
+            slString.Add("Gabriel");
+            slString.Add("Enzo");
+            slString.Add("Anna");
+
+            // populate int sorted list # 1
+            sl1.Add(5);
+            sl1.Add(1);
+            sl1.Add(7);
+            sl1.Add(55);
+            sl1.Add(20);
+            sl1.Add(9);
+
+            // populate int sorted list # 2
+            sl2.Add(6);
+            sl2.Add(2);
+            sl2.Add(8);
+            sl2.Add(56);
+            sl2.Add(21);
+            sl2.Add(10);
+
+            // display sorted list # 1
+            sl1.DisplayInt();
+
+            // display sorted list # 2
+            sl2.DisplayInt();
+
+            // display sorted list (string)
+            slString.Display();
+
+            // merge list 1 and 2
+            SortedList slFinal = Merge(sl1, sl2);
+
+            // display merged list (this is sorted)
+            slFinal.DisplayInt();
+
+            
+        }
+
+        static SortedList Merge(SortedList list1, SortedList list2)
+        {
+            SortedList final = list1;
+            final.SetName("mergedList");
+
+            ListNode tempNode = list2.firstNode;
+
+            while (tempNode != null)
+            {
+                ListNode tempNode2 = new ListNode(tempNode.IntData);
+                ListNode currNode = final.firstNode;
+                ListNode prevNode = null;
+
+                while (currNode != null && currNode.IntData < tempNode2.IntData)
+                {
+                    prevNode = currNode;
+                    currNode = currNode.Next;
+                }
+
+                if (prevNode == null)
+                {
+                    final.firstNode = tempNode2;
+                    final.firstNode.Next = currNode;
+                }
+                else
+                {
+                    prevNode.Next = tempNode2;
+                    tempNode2.Next = currNode;
+                }
+
+                tempNode = tempNode.Next;
+            }
+
+
+            return final;
+        }
+    }
+}
---
+++ 300910584(goh)_ASS_4/Goh_Q2/Goh_Q2/SortedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedListLibrary;
+using System.Collections;
+
+namespace Goh_Q2
+{
+    // This is a class that implements the List class under the LinkedListLibrary project
+    public class SortedList : LinkedListLibrary.List
+    {
+        public SortedList(string listName) : base(listName)
+        {
+
+        }
+
+        // method to add a new integer item
+        public void Add(int addItem)
+        {
+            // check if list is empty
+            if (IsEmpty())
+            {
+                firstNode = lastNode = new ListNode(addItem);
+            }
+            else
+            {
+                ListNode tempNode = new ListNode(addItem);
+                ListNode currNode = firstNode;
+                ListNode prevNode = null;
+
+                // find the correct position to insert the new node
+                while(currNode != null && currNode.IntData < addItem)
+                {
+                    prevNode = currNode;
+                    currNode = currNode.Next;
+                }
+
+                // if previous node is null, that means there is only 1 node that currently exists
+                if(prevNode == null)
+                {
+                    firstNode = tempNode;
+                    firstNode.Next = currNode;
+                }
+                else 
+                {
+                    prevNode.Next = tempNode; // set the newly inserted node as the previous node's Next property
+                    tempNode.Next = currNode; // set the current node as the newly inserted node's Next property
+                }
+            }
+            
+        }
+
+        // method to add a new string item
+        public void Add(string addItem)
+        {
+            if (IsEmpty())
+            {
+                firstNode = lastNode = new ListNode(addItem);
+            }
+            else
+            {
+                ListNode tempNode = new ListNode(addItem);
+                ListNode currNode = firstNode;
+                ListNode prevNode = null;
+
+                while (currNode != null && currNode.Data.ToString().CompareTo(addItem) < 0)
+                {
+                    prevNode = currNode;
+                    currNode = currNode.Next;
+                }
+
+                if (prevNode == null)
+                {
+                    firstNode = tempNode;
+                    firstNode.Next = currNode;
+                }
+                else
+                {
+                    prevNode.Next = tempNode;
+                    tempNode.Next = currNode;
+                }
+            }
+
+        }
+        
+    }
+}
