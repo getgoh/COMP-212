313bc7f9bc3f5a1bdfb942a4a99e4e4e011d1af0
getgoh <blujaygo@gmail.com>
2017-03-29 12:20:52 -0400

    Ass 5 - Question 2 almost done

---
+++ 300910584(goh)_ASS_5/Q2/frmSearch.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Q2
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateInt = new System.Windows.Forms.Button();
+            this.btnGenerateDouble = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerateInt
+            // 
+            this.btnGenerateInt.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerateInt.Name = "btnGenerateInt";
+            this.btnGenerateInt.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerateInt.TabIndex = 0;
+            this.btnGenerateInt.Text = "Generate Int";
+            this.btnGenerateInt.UseVisualStyleBackColor = true;
+            this.btnGenerateInt.Click += new System.EventHandler(this.btnGenerateInt_Click);
+            // 
+            // btnGenerateDouble
+            // 
+            this.btnGenerateDouble.Location = new System.Drawing.Point(103, 12);
+            this.btnGenerateDouble.Name = "btnGenerateDouble";
+            this.btnGenerateDouble.Size = new System.Drawing.Size(105, 23);
+            this.btnGenerateDouble.TabIndex = 1;
+            this.btnGenerateDouble.Text = "Generate Double";
+            this.btnGenerateDouble.UseVisualStyleBackColor = true;
+            this.btnGenerateDouble.Click += new System.EventHandler(this.btnGenerateDouble_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(59, 41);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(149, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 69);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSearch.Size = new System.Drawing.Size(196, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(13, 99);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(195, 176);
+            this.txtDisplay.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search";
+            // 
+            // frmSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnGenerateDouble);
+            this.Controls.Add(this.btnGenerateInt);
+            this.Name = "frmSearch";
+            this.Text = "Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateInt;
+        private System.Windows.Forms.Button btnGenerateDouble;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 300910584(goh)_ASS_5/Q2/frmSearch.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2
+{
+    public partial class frmSearch : Form
+    {
+        int[] intValues = new int[10];
+        double[] doubleValues = new double[10];
+        string searchStr = "";
+
+        Random rand;
+
+        public frmSearch()
+        {
+            InitializeComponent();
+            rand = new Random();
+
+            generateInt();
+            generateDouble();
+
+            displayValues();
+        }
+
+        private void displayValues()
+        {
+            txtDisplay.Text = "int values: ";
+            txtDisplay.Text += string.Join(", ", intValues) + Environment.NewLine + Environment.NewLine;
+
+            txtDisplay.Text += "double values: ";
+            txtDisplay.Text += string.Join(", ", doubleValues) + Environment.NewLine;
+        }
+
+        private void btnGenerateInt_Click(object sender, EventArgs e)
+        {
+            generateInt();
+        }
+
+        private void generateInt()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                intValues[x] = rand.Next(20);
+            }
+            displayValues();
+        }
+
+        private void btnGenerateDouble_Click(object sender, EventArgs e)
+        {
+            generateDouble();
+        }
+
+        private void generateDouble()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                doubleValues[x] = Math.Round(rand.NextDouble() * 10, 2);
+            }
+            displayValues();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchStr = txtSearch.Text.Trim();
+            int index = Search<int>(int.Parse(searchStr), intValues);
+            if (index == -1)
+            {
+                index = Search<double>(double.Parse(searchStr), doubleValues);
+                if(index == -1)
+                {
+                    MessageBox.Show(searchStr + " not found in int or double array");
+                }
+                else
+                {
+                    MessageBox.Show(searchStr + " found in double array at index " + index);
+                }
+            }
+            else
+            {
+                MessageBox.Show(searchStr + " found in int array at index " + index);                     
+            }
+        }
+
+        private int Search<T>(T searchParam, T[] array) where T : IComparable<T>
+        {
+            int index = -1;
+            int ctr = 0;
+
+            // search through array
+            foreach(var i in array)
+            {
+                if(searchParam.CompareTo(i) > 0)
+                {
+                    index = ctr;
+                    break;
+                }
+                ctr++;
+            }
+            
+            return index;
+        }
+    }
+}
